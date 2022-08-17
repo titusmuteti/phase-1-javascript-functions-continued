e40d521d09cde2fd2f46784a1baf630f6d066468
Titus Mumo <titusmumo@gmail.com>
2022-08-18 00:21:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(message = 'roller-skate') {
+    return `This Saturday, I want to ${message}!`; 
+}
+
+function mondayWork (message = 'go to the office') {
+    return `This Monday, I will ${message}.`;
+}
+
+function wrapAdjective (wrapped='*') {
+    return function (msg = 'special') {
+        return `You are ${wrapped}${msg}${wrapped}!`
+    }
+}
